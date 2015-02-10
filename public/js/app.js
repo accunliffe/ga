ba5d9509babe5d25a056aca6400a7c7f814523b2
Andrew Cunliffe <rhinocapitalmgmt@gmail.com>
2015-02-10 00:04:38 -0800

    starting to get routes

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,13 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('ldga',['ui.bootstrap']);
+angular.module('ldga',['ui.bootstrap','ngRoute']).
+  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/view1', {templateUrl: 'partials/partial1', controller: MyCtrl1});
+    $routeProvider.when('/view2', {templateUrl: 'partials/partial2', controller: MyCtrl2});
+    $routeProvider.when('/view3', {templateUrl: 'partials/partial3', controller: MyCtrl3});
+    $routeProvider.when('/view4', {templateUrl: 'partials/partial4', controller: MyCtrl4});
+    $routeProvider.when('myModalContent', {templateUrl: 'partials/myModalContent', controller: ModalCtrl})
+    $routeProvider.otherwise({redirectTo: '/view1'});
+    $locationProvider.html5Mode(true);  
+  }]);
